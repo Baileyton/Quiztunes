a332c751025966c61cee52f451d28a3bb4bd2bb4
Baileyton <baileyton@naver.com>
2024-04-28 17:13:55 +0900

    first commit

---
+++ src/main/java/com/example/quiztunes/QuiztunesApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiztunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiztunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiztunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quiztunes/QuiztunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiztunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiztunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
